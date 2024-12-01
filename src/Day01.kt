6d6cd427517cd3ce00689c30dcd5dab80fe53d6d
Abdul Khaliq <Akhunzaada@gmail.com>
2024-12-01 15:03:22 +0500

    Day 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,44 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val listOne = mutableListOf<Int>()
+        val listTwo = mutableListOf<Int>()
+        input.forEach { line ->
+            line.split(" ").let {
+                listOne.add(it.first().toInt())
+                listTwo.add(it.last().toInt())
+            }
+        }
+        listOne.sort()
+        listTwo.sort()
+
+        var result = 0
+        for (i in 0 until listOne.size) {
+            result += abs(listOne[i].minus(listTwo[i]))
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val listOne = mutableListOf<Int>()
+        val listTwo = mutableMapOf<Int, Int>()
+        input.forEach { line ->
+            line.split(" ").let {
+                listOne.add(it.first().toInt())
+                listTwo[it.last().toInt()] = (listTwo[it.last().toInt()] ?: 0).plus(1)
+            }
+        }
+
+        return listOne.sumOf {
+            it.times(listTwo[it] ?: 0)
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
